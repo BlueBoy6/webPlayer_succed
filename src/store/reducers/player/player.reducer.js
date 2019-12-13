caefa5676c59093263ed2ad0b8a7d95abfffce0b
David Simoes Silva <dsimoes-silva@intuitiv-technology.com>
2019-12-13 16:51:58 +0100

    patch bug on useEffect but need stoplay on end video

--- src/store/reducers/player/player.reducer.js
+++ src/store/reducers/player/player.reducer.js
@@ -1,24 +1,24 @@
 import {
-	PLAY_PAUSE,
-	VOLUME_CHANGE,
-	TOGGLE_FULLSCREEN
-} from '../../actions/types';
+  PLAY_PAUSE,
+  VOLUME_CHANGE,
+  TOGGLE_FULLSCREEN
+} from "../../actions/types";
 
 export const currentPlayerState = {
-	volume: 25,
-	isPlaying: false,
-	isFullScreen: false
+  volume: 25,
+  isPlaying: false,
+  isFullScreen: false
 };
 
 export function playerReducer(state = currentPlayerState, action) {
-	switch (action.type) {ya
-		case TOGGLE_FULLSCREEN:
-			return { ...state, isFullScreen: !action.payload };
-		case VOLUME_CHANGE:
-			return { ...state, volume: action.payload };
-		case PLAY_PAUSE:
-			return { ...state, isPlaying: action.payload };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case TOGGLE_FULLSCREEN:
+      return { ...state, isFullScreen: !action.payload };
+    case VOLUME_CHANGE:
+      return { ...state, volume: action.payload };
+    case PLAY_PAUSE:
+      return { ...state, isPlaying: action.payload };
+    default:
+      return state;
+  }
 }
